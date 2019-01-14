eb90c49e9341f28da4431245a470eff494817d8e
Nairodie <42880967+Nairodie@users.noreply.github.com>
2019-01-13 22:46:01 -0600

    Create Euler2BruteForce.cs

---
+++ Euler2BruteForce.cs
@@ -0,0 +1,27 @@
+using System;
+
+//less than four million, find the sum of the even-valued terms.					
+public class Program
+{
+	public static void Main()
+	{	
+		int fib1 = 1;
+		int fib2 = 1;
+		int result = 0;
+		int sum = 0;
+		
+		while(result < 4000000)
+		{
+			if (result % 2 == 0)
+			{
+				sum += result;
+			}
+			
+			//F(n)=F(n-1)+F(n-2)
+			result = fib1 + fib2;
+			fib2 = fib1;
+			fib1 = result;
+		}
+		Console.WriteLine(sum);		
+	}
+}
