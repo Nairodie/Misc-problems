d3ce988f7b3a06d2b5a46ab2629538e85188e8ea
Nairodie <42880967+Nairodie@users.noreply.github.com>
2019-01-15 11:38:48 -0600

    Update LinqFindingFirstNonRepeating.cs

--- LinqFindingFirstNonRepeating.cs
+++ LinqFindingFirstNonRepeating.cs
@@ -5,11 +5,10 @@ public class Program
 {
 	public static void Main()
 	{
-		string input = "Stress";
-		
+		string input = "Stress";		
 		var result = input
 			.GroupBy(x => x.ToString(),StringComparer.CurrentCultureIgnoreCase)
-			.Where(y => y.Count()==1)
+			.Where(y => y.Count() == 1)
 			.Select(y => y.Key)
 			.FirstOrDefault();
 		Console.WriteLine(result);
