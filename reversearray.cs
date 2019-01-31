d52056c49d7291103c5f0970e33456ae293bdee2
Nairodie <42880967+Nairodie@users.noreply.github.com>
2019-01-31 16:57:36 -0600

    Update reversearray.cs

--- reversearray.cs
+++ reversearray.cs
@@ -2,20 +2,19 @@ using System;
 					
 public class Program
 {
-	public static void Main()
+public static void Main()
+{
+	//create int array
+	int[] myArray = {1,3,5,7,9,11};
+	
+	//search through new array created
+	for (int i = 0; i < myArray.Length / 2; i++)
 	{
-    //create int array
-		int[] myArray = {1,3,5,7,9,11};
-		
-    //search through new array created
-		for (int i = 0; i < myArray.Length / 2; i++)
-		{
-    //create temp variable with the results from the search
-			int temp = myArray[i];
-			myArray[i] = myArray[myArray.Length - i - 1];
-			myArray[myArray.Length - i - 1] = temp;
-		}
-			Console.WriteLine(string.Join(",", myArray));
-		
+		//create temp variable with the results from the search
+		int temp = myArray[i];
+		myArray[i] = myArray[myArray.Length - i - 1];
+		myArray[myArray.Length - i - 1] = temp;
+	}
+	Console.WriteLine(string.Join(",", myArray));
 	}
 }
