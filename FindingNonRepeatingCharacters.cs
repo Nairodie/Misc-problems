1fbf6a88f3bf3a7cdae3a842618231707bc44e7b
Nairodie <42880967+Nairodie@users.noreply.github.com>
2019-02-08 19:04:02 -0600

    Update FindingNonRepeatingCharacters.cs

--- FindingNonRepeatingCharacters.cs
+++ FindingNonRepeatingCharacters.cs
@@ -13,6 +13,8 @@ public class Program
 			//LastIndexOf searches in reverse, from right to left part of string
 			if (output.IndexOf(value) == output.LastIndexOf(value))
 			{
+				// force the letter you want returned
+				if (value.ToString() == "t")
 				Console.WriteLine(value);
 			}
 		}
