613aacf975a36570b977ea2b045a05e06de9f458
Nairodie <42880967+Nairodie@users.noreply.github.com>
2019-03-29 21:56:40 -0500

    Update FibEvenNumbers.cs

--- FibEvenNumbers.cs
+++ FibEvenNumbers.cs
@@ -2,21 +2,20 @@ using System;
 					
 public class Program
 {
-	//fibonci even numbers under 4000000 added together
 	public static void Main()
-	{
-		//variables 
-		int fib1 = 1, fib2 = 1, result = 0, sum = 0;
+	{ 
+		int fib1 = 1;
+		int fib2 = 1;
+		int result = 0;
+		int sum = 0;
 		
-    		//loop through all number under 4000000
 		while (result < 4000000)
 		{
 			if (result % 2 == 0)
 			{
-			sum = sum + result;
+				sum += result;
 			}
 			
-			//F(n)=F(n-1)+F(n-2)
 			result = fib1 + fib2;
 			fib2 = fib1;
 			fib1 = result;
