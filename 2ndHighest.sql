76f4704fa06450014c6b9ca3fe5ba76f695f0b8b
Nairodie <42880967+Nairodie@users.noreply.github.com>
2019-04-24 19:24:50 -0500

    Update 2ndHighest.sql

--- 2ndHighest.sql
+++ 2ndHighest.sql
@@ -1,5 +1,5 @@
-SELECT Max(Salary)
-FROM Employee
+// Query to find second highest salary of Employee using subquery
+SELECT Max(Salary) FROM Employee
 Where Salary NOT IN (
   Select MAX(Salary)
     FROM Employee
