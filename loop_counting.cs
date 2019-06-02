3964393ca4e4340f2d1285ecca18f0e094d4a16e
Nairodie <42880967+Nairodie@users.noreply.github.com>
2019-06-02 17:22:29 -0500

    Update loop_counting.cs

--- loop_counting.cs
+++ loop_counting.cs
@@ -4,16 +4,16 @@ public class Program
 {
 	public static void Main()
 	{
-		for (int i=1; i < 5; i++)
-			{
-			for (int i = 1; i <= 10; i++)
-			{
+		for (int j = 1; j < 5; j++)
+		{
+		for (int i = 1; i <= 10; i++)
+		{
 			Console.WriteLine(i);
-			}
-			for (int i = 10; i >= 1; i--)
-			{
-			Console.WriteLine(i);
-			}
 		}
+		for (int i = 10; i >= 1; i--)
+		{
+			Console.WriteLine(i);
+		}
+	}
 	}
 }
