a01e1e973e678620e9c4b1dfe2e44c8c7d0fd9ac
Nairodie <42880967+Nairodie@users.noreply.github.com>
2019-06-14 22:19:48 -0500

    Update FindingNonRepeatingCharacters.cs

--- FindingNonRepeatingCharacters.cs
+++ FindingNonRepeatingCharacters.cs
@@ -4,16 +4,12 @@ public class Program
 {
 	public static void Main()
 	{
-		// find first non-repeating letter in word.
 		string output = "stress";
 		
-		//loop through each letter in the word
 		foreach (var value in output)
 		{
-			//LastIndexOf searches in reverse, from right to left part of string
 			if (output.IndexOf(value) == output.LastIndexOf(value))
 			{
-				// force the letter you want returned
 				if (value.ToString() == "t")
 				Console.WriteLine(value);
 			}
