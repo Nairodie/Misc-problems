29cc251236627e125af6704f2a57d760457c4af8
Nairodie <42880967+Nairodie@users.noreply.github.com>
2020-01-08 06:53:24 -0600

    Update FindingNonRepeatingCharacters.cs

--- FindingNonRepeatingCharacters.cs
+++ FindingNonRepeatingCharacters.cs
@@ -4,13 +4,15 @@ public class Program
 {
 	public static void Main()
 	{
-		string output = "stress";
+		string output = "Stress";
 		
 		foreach (var value in output)
 		{
+			//using index of to compare first and last of the string
 			if (output.IndexOf(value) == output.LastIndexOf(value))
 			{
-				if (value.ToString() == "t")
+				//needs to be manipulated as returns blank string if double value found (s)
+				if (value.ToString() == "s")
 				Console.WriteLine(value);
 			}
 		}
